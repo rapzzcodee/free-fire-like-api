39e57904934c819746f4a459bd4225f2461cde4b
rapzzcodee <rapzcode@gmail.com>
2025-12-26 19:34:29 +0700

    Add URL for ID in URLS_LIKE and URLS_INFO

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ URLS_LIKE = {"IND":"https://client.ind.freefiremobile.com/LikeProfile",
             "BR":"https://client.us.freefiremobile.com/LikeProfile",
             "US":"https://client.us.freefiremobile.com/LikeProfile",
             "SAC":"https://client.us.freefiremobile.com/LikeProfile",
+            "ID":"https://gin.freefiremobile.com/LikrProfile",
             "NA":"https://client.us.freefiremobile.com/LikeProfile"}
 
 
@@ -11,6 +12,7 @@ URLS_INFO = {"IND":"https://client.ind.freefiremobile.com/GetPlayerPersonalShow"
             "BR":"https://client.us.freefiremobile.com/GetPlayerPersonalShow",
             "US":"https://client.us.freefiremobile.com/GetPlayerPersonalShow",
             "SAC":"https://client.us.freefiremobile.com/GetPlayerPersonalShow",
+            "ID":"https://gin.freefiremobile.com/GetPlayerPersonalShow",
             "NA":"https://client.us.freefiremobile.com/GetPlayerPersonalShow"}
 
 
@@ -19,4 +21,5 @@ FILES = {"IND":"token_ind.json",
              "BR":"token_br.json",
              "US":"token_br.json",
              "SAC":"token_br.json",
+             "ID":"token_id.json",
              "NA":"token_br.json"}
